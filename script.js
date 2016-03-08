012ccd0621ab9a08c0bc479e2cad5b9e96d2af22
Clarissa Stark <clarissa.stark@gmail.com>
2016-03-08 12:59:51 -0500

    fixed tileId error

--- script.js
+++ script.js
@@ -1,67 +1,71 @@
 
 // *more than five variables
 
-var colors = ["red", "blue", "orange", "green", "purple", "yellow", "red", "blue", "orange", "green", "purple", "yellow"];
-var matchedTiles = 0;
-var memoryValues = [];
-var memoryTileIds = [];
-var numTries = 0;
-var startBtn = document.querySelector("#start");
-var gameTimer;
+var game = {
+  tileIds: [],
+  colors: ["red", "blue", "orange", "green", "purple", "yellow", "red", "blue", "orange", "green", "purple", "yellow"],
+  matchedTiles: 0,
+  values: [],
+  numTries: 0
+};
 
+// put function in object
 // This starts the game when user pushes the big red button
 
-startBtn.addEventListener("click", newBoard);
+$("#start").on("click", newBoard);
 
 // This sets up the board
 
 function newBoard(){
   // this makes sure the completed grid is removed at the end of a game
-  var parentDiv = document.getElementById("grid");
+  var parentDiv = $("#grid");
   while (parentDiv.firstChild){
-    parentDiv.removeChild(parentDiv.firstChild);
+    parentDiv.remove(parentDiv.firstChild);
   }
-  matchedTiles = 0;
-  colors.shuffleDeck();
+  // this resets the matched tiles to 0 for a new game
+  game.matchedTiles = 0;
+  game.colors.shuffleDeck();
   // this draws the game board
-  for (var i = 0; i < colors.length; i++){
+  for (var i = 0; i < game.colors.length; i++){
     // this could be jQuery
     var tile = document.createElement("div");
     tile.id = "tile_" + i;
-    tile.setAttribute("data-color", colors[i]);
+    tile.setAttribute("data-color", game.colors[i]);
     tile.addEventListener("click", function(){
-      memoryFlipTile(this);
+      var tile = this;
+      flipTile(tile);
     });
-    document.getElementById("grid").appendChild(tile);
+    $("#grid").append(tile);
   }
 }
 
 // This flips over cards and matches or flips them back over, and ends the game when won or lost after 12 tries
 
-
 // *this function is really long
 
-function memoryFlipTile(tile){
+function flipTile(tile){
   var val = tile.getAttribute("data-color");
   tile.style.backgroundColor = val;
-  memoryValues.push(val);
-  memoryTileIds.push(tile.id);
-  numTries += 0.5;
-  if (memoryValues.length == 2){
-    if (isAndIsMatch(memoryValues[0],memoryValues[1]) === false){
+  game.values.push(val);
+  console.log(tile.id);
+  game.tileIds.push(tile.id);
+  game.numTries += 0.5;
+  // this feels like it could be simplified into another function?
+  if (game.values.length == 2){
+    if (isAndIsMatch(game.values[0],game.values[1]) === false){
       this.setTimeout(flipBack, 700);
-    }else if (isAndIsMatch(memoryValues[0],memoryValues[1]) === true){
-      matchedTiles += 2;
-      memoryValues = [];
-      memoryTileIds = [];
-      if (matchedTiles === colors.length){
+    }else if (isAndIsMatch(game.values[0],game.values[1]) === true){
+      game.matchedTiles += 2;
+      game.values = [];
+      game.tileIDs = [];
+      if (game.matchedTiles === game.colors.length){
         alert("You've won! Click 'Let's play!' to play again.");
-        numTries = 0;
+        game.numTries = 0;
       }
     }
-  }else if(gameTimer === 0){
+  }else if(game.numTries > 12){
     alert("You're out of tries!");
-    numTries = 0;
+    game.numTries = 0;
   }
 }
 
@@ -69,12 +73,12 @@ function memoryFlipTile(tile){
 
 function flipBack(){
   // *can this be condensed at all?
-  var tile1 = document.getElementById(memoryTileIds[0]);
-  var tile2 = document.getElementById(memoryTileIds[1]);
+  var tile1 = document.getElementById(game.tileIds[0]);
+  var tile2 = document.getElementById(game.tileIds[1]);
   tile1.style.backgroundColor = "#247bbe";
   tile2.style.backgroundColor = "#247bbe";
-  memoryValues = [];
-  memoryTileIds = [];
+  game.values = [];
+  game.tileIDs = [];
 }
 
 // This deteremines if two items are a match, excluding 'undefined', "" & null
@@ -83,7 +87,7 @@ function isAndIsMatch(a,b){
   return !!a && !!b && a === b;
 }
 
-// This randomizes the colors on the back of the cards in the grid
+// This randomizes the color pairs on the back of the cards in the grid
 
 Array.prototype.shuffleDeck = function(){
   for (var i = this.length - 1; i > 0; i--) {
